46157ec7695c51fa693cdf0df0be62a87e3a0e50
sirius6323 <sirius6323@gmail.com>
2021-05-05 00:06:36 -0700

    Imported React/ReactDOM, added React Component

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import statement to bundle './index.scss'
+import './index.scss';
+
+// Main component (will eventually use all others)
+class MyIndieFixApplication extends React.Component {
+	render() {
+		return (
+			<div className='my-indie'>
+				<div>Greetings from IndieFix!!!</div>
+			</div>
+		);
+	}
+}
+
+// Finds the root of my app
+const container = document.getElementByClassName('app-container')[0];
+
+// React renders my app in the root DOM element
+ReactDOM.render(React.createElement(MyIndieFixApplication), container);
