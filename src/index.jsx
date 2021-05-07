faf17e7f7d8eef353a677a3a739a35cd4e8e4344
sirius6323 <sirius6323@gmail.com>
2021-05-06 20:54:45 -0700

    Imported MainView Component

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainView from './components/main-view/main-view';
 
 // Import statement to bundle './index.scss'
 import './index.scss';
