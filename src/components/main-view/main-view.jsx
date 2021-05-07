d44cce1a75d925e2a76d2f1597ac195b0c9f8475
sirius6323 <sirius6323@gmail.com>
2021-05-06 20:39:21 -0700

    Created main-view component

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+	render() {
+		return (
+			<div className='main-view'>
+				<div>ParaNorman</div>
+				<div>The Life Aquatic with Steve Zissou</div>
+				<div>The Isle of Dogs</div>
+			</div>
+		);
+	}
+}
