72ee83f21f3e6993ceb07bd2e7387aaaedb897ee
sirius6323 <sirius6323@gmail.com>
2021-05-07 21:21:33 -0700

    Added click event to MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
 	render() {
-		const { movieData } = this.props;
-		return <div className='movie-card'>{movieData.Title}</div>;
+		const { movieData, onMovieClick } = this.props;
+		return (
+			<div
+				className='movie-card'
+				onClick={() => {
+					onMovieClick(movie);
+				}}
+			>
+				{movieData.Title}
+			</div>
+		);
 	}
 }
