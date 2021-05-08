f910e398c2dbbe7a68fe230f7f0ddd3fb72060a8
sirius6323 <sirius6323@gmail.com>
2021-05-07 22:02:20 -0700

    Renamed onMovieClick parameter

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 export class MovieCard extends React.Component {
 	render() {
 		const { movieData, onMovieClick } = this.props;
+
 		return (
 			<div
 				className='movie-card'
 				onClick={() => {
-					onMovieClick(movie);
+					onMovieClick(movieData);
 				}}
 			>
 				{movieData.Title}
