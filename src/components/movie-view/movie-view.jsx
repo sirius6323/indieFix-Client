5571a8fb6c719c99565bd8562a0061974a0ef522
sirius6323 <sirius6323@gmail.com>
2021-05-08 07:46:14 -0700

    Added spaces for better readability when displaying movie info

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -11,19 +11,19 @@ export class MovieView extends React.Component {
 					<img src={movie.ImageURL} />
 				</div>
 				<div className='movie-title'>
-					<span className='label'>Title:</span>
+					<span className='label'>Title: </span>
 					<span className='value'>{movie.Title}</span>
 				</div>
 				<div className='movie-description'>
-					<span className='label'>Description:</span>
+					<span className='label'>Description: </span>
 					<span className='value'>{movie.Description}</span>
 				</div>
 				<div className='movie-genre'>
-					<span className='label'>Genre:</span>
+					<span className='label'>Genre: </span>
 					<span className='value'>{movie.Genre}</span>
 				</div>
 				<div className='movie-director'>
-					<span className='label'>Director:</span>
+					<span className='label'>Director: </span>
 					<span className='value'>{movie.Director}</span>
 				</div>
 				<button
