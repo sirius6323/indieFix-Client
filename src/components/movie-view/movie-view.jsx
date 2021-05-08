9736cfa4c91a9decc61c318050d923a986898bd6
sirius6323 <sirius6323@gmail.com>
2021-05-07 22:16:47 -0700

    Added onBackClick event handler to button in MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -18,7 +18,13 @@ export class MovieView extends React.Component {
 					<span className='label'>Description:</span>
 					<span className='value'>{movie.Description}</span>
 				</div>
-				<button>Back</button>
+				<button
+					onClick={() => {
+						onBackClick(null);
+					}}
+				>
+					Back
+				</button>
 			</div>
 		);
 	}
