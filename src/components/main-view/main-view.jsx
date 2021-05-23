e5b371af6180cf51522229d482ecb5c826c587b5
sirius6323 <sirius6323@gmail.com>
2021-05-23 16:51:17 -0700

    Imported LoginView Component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
