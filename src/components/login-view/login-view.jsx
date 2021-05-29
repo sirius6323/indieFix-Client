c44a71cb5e07d4c49c3f1a48f081b739f5652996
sirius6323 <sirius6323@gmail.com>
2021-05-28 23:22:17 -0700

    Added React Component styling to login form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import './login-view.scss';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 export function LoginView(props) {
 	const [username, setUsername] = useState('');
@@ -17,53 +16,62 @@ export function LoginView(props) {
 	};
 
 	return (
-		<Form>
-			<h3>Login</h3>
-			<Form.Group controlId='formUsername' className='mb-3'>
-				<Form.Label className='login-input'>Username</Form.Label>
-				<Form.Control
-					size='md'
-					type='text'
-					onChange={(e) => setUsername(e.target.value)}
-					placeholder='Enter Username'
-				/>
-				<Form.Text className='text-muted'>
-					We'll never share your user account info with anyone else.
-				</Form.Text>
-			</Form.Group>
+		<Container>
+			<div className='d-flex align-items-center justify-content-center customHeight'>
+				<Form className='custom-form custom-box-shadow'>
+					<h3 className='d-flex mb-3 text-align-center justify-content-center login-title'>
+						Login to indieFix
+					</h3>
+					<Form.Group controlId='formUsername' className='mb-3'>
+						<Form.Label className='form-text'>Username</Form.Label>
+						<Form.Control
+							size='md'
+							type='text'
+							onChange={(e) => setUsername(e.target.value)}
+							placeholder='Enter Username'
+						/>
+						<Form.Text className='text-muted form-text'>
+							We'll never share your user account info with anyone else.
+						</Form.Text>
+					</Form.Group>
 
-			<Form.Group controlId='formPassword'>
-				<Form.Label>Password</Form.Label>
-				<Form.Control
-					size='md'
-					type='password'
-					onChange={(e) => setPassword(e.target.value)}
-					placeholder='Enter Password'
-				/>
-				<Form.Text className='text-muted'>
-					Your password requires a minimum of 8 characters.
-				</Form.Text>
-			</Form.Group>
-			<div className='d-grid gap-2'>
-				<Button
-					variant='primary'
-					type='submit'
-					size='lg'
-					onClick={handleSubmit}
-				>
-					Submit
-				</Button>
-				<Button type='secondary' size='lg' onClick={props.toggleRegister}>
-					Register
-				</Button>
+					<Form.Group controlId='formPassword' className='mb-3'>
+						<Form.Label className='form-text'>Password</Form.Label>
+						<Form.Control
+							size='md'
+							type='password'
+							onChange={(e) => setPassword(e.target.value)}
+							placeholder='Enter Password'
+						/>
+						<Form.Text className='text-muted form-text'>
+							Your password requires a minimum of 8 characters.
+						</Form.Text>
+					</Form.Group>
+					<div className='d-grid gap-2 button-height'>
+						<Button
+							variant='info primary'
+							type='submit'
+							size='sm'
+							onClick={handleSubmit}
+							block
+						>
+							Submit
+						</Button>
+						<Button
+							variant='dark'
+							type='secondary'
+							size='sm'
+							onClick={props.toggleRegister}
+							block
+						>
+							Register
+						</Button>
+					</div>
+				</Form>
 			</div>
-		</Form>
+		</Container>
 	);
 }
-
-/* export function Button({ label }) {
-	return <Button className='login-button'>{label}</Button>;
-} */
 
 LoginView.propTypes = {
 	user: PropTypes.shape({
