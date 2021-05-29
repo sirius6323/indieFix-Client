97191cb12f04ecccb82883dc27d92f72858202b4
sirius6323 <sirius6323@gmail.com>
2021-05-29 01:32:48 -0700

    Added Row and Col to main-view component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -75,30 +75,30 @@ class MainView extends React.Component {
 		if (movies.length === 0) return <div className='main-view' />;
 
 		return (
-			<div className='main-view'>
+			<Row className='main-view justify-content-md-center'>
 				{selectedMovie ? (
-					<Row className='justify-content-md-center'>
-						<Col md={8}>
-							<MovieView
-								movie={selectedMovie}
-								onBackClick={(newSelectedMovie) => {
+					<Col md={8}>
+						<MovieView
+							movie={selectedMovie}
+							onBackClick={(newSelectedMovie) => {
+								this.setSelectedMovie(newSelectedMovie);
+							}}
+						/>
+					</Col>
+				) : (
+					movies.map((movie) => (
+						<Col md={4}>
+							<MovieCard
+								key={movie._id}
+								movie={movie}
+								onMovieClick={(newSelectedMovie) => {
 									this.setSelectedMovie(newSelectedMovie);
 								}}
 							/>
 						</Col>
-					</Row>
-				) : (
-					movies.map((movie) => (
-						<MovieCard
-							key={movie._id}
-							movie={movie}
-							onMovieClick={(newSelectedMovie) => {
-								this.setSelectedMovie(newSelectedMovie);
-							}}
-						/>
 					))
 				)}
-			</div>
+			</Row>
 		);
 	}
 }
