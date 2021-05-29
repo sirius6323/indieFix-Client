35011a788fb4a1a50d23b9cd82e4d7be9ab4cda7
sirius6323 <sirius6323@gmail.com>
2021-05-29 02:20:06 -0700

    Added React Bootstrap styling to MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Button, Card, CardDeck } from 'react-bootstrap';
 
 export class MovieCard extends React.Component {
 	render() {
 		const { movie, onMovieClick } = this.props;
 
 		return (
-			<div
-				onClick={() => {
-					onMovieClick(movie);
-				}}
-				className='movie-card'
-			>
-				{movie.Title}
-			</div>
+			<CardDeck>
+				<Card className='mb-2' border='info' style={{ width: '18rem' }}>
+					<Card.Img variant='top' src={movie.ImageURL / '100px160'} />
+					<Card.Body>
+						<Card.Title className='text-center'>{movie.Title}</Card.Title>
+						<Card.Text>{movie.Description}</Card.Text>
+						<Button
+							onClick={() => onMovieClick(movie)}
+							variant='danger link'
+							size='md'
+							block
+						>
+							Open
+						</Button>
+					</Card.Body>
+				</Card>
+			</CardDeck>
 		);
 	}
 }
