f3ba16af9834ec377714ceea431f26fd34451b8f
sirius6323 <sirius6323@gmail.com>
2021-05-30 01:52:27 -0700

    Refactored code for registeration-view

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import './registration-view.scss';
 
 export function RegistrationView(props) {
@@ -17,68 +18,115 @@ export function RegistrationView(props) {
 	};
 
 	return (
-		<form className='register-form'>
-			<label className='register-input'>
-				First Name:
-				<input
-					type='text'
-					value={firstName}
-					onChange={(e) => setFirstName(e.target.value)}
-				/>
-			</label>
-			<label className='register-input'>
-				Last Name:
-				<input
-					type='text'
-					value={lastName}
-					onChange={(e) => setLastName(e.target.value)}
-				/>
-			</label>
-			<label className='register-input'>
-				Birthday:
-				<input
-					type='text'
-					value={birthday}
-					onChange={(e) => setBirthday(e.target.value)}
-				/>
-			</label>
-			<label className='register-input'>
-				Username:
-				<input
-					type='text'
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-			</label>
-			<label className='register-input'>
-				Password:
-				<input
-					type='text'
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-			</label>
-			<label className='register-input'>
-				Email:
-				<input
-					type='text'
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-			</label>
-			<span>
-				<button type='button' onClick={handleSubmit}>
-					Submit
-				</button>
-				<button
-					onClick={() => {
-						onBackClick(null);
-					}}
-				>
-					Back
-				</button>
-			</span>
-		</form>
+		<Container>
+			<div className='d flex align-items-center justify-content-center customHeight'>
+				<Form className='custom-form'>
+					<h3 className='d-flex mb-3 text-align-center justify-content-center font-weight-bold register'>
+						Register to indieFix
+					</h3>
+					<Row>
+						<Col>
+							<Form.Group controlId='registerFirstName' className='mb-3'>
+								<Form.Label>First Name</Form.Label>
+								<Form.Control
+									type='text'
+									value={firstName}
+									placeholder='First Name'
+									onChange={(e) => setFirstName(e.target.value)}
+								/>
+							</Form.Group>
+						</Col>
+						<Col>
+							<Form.Group controlId='registerLastName' className='mb-3'>
+								<Form.Label>Last Name</Form.Label>
+								<Form.Control
+									type='text'
+									value={lastName}
+									placeholder='Last Name'
+									onChange={(e) => setLastName(e.target.value)}
+								/>
+							</Form.Group>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Form.Group controlId='registerBirthday' className='mb-3'>
+								<Form.Label>Birthday</Form.Label>
+								<Form.Control
+									type='text'
+									value={birthday}
+									placeholder='MM/DD/YY'
+									onChange={(e) => setBirthday(e.target.value)}
+								/>
+							</Form.Group>
+						</Col>
+						<Col>
+							<Form.Group controlId='registerUserName' className='mb-3'>
+								<Form.Label>Username</Form.Label>
+								<Form.Control
+									type='text'
+									value={username}
+									placeholder='Username'
+									onChange={(e) => setUsername(e.target.value)}
+								/>
+							</Form.Group>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Form.Group controlId='registerPassword' className='mb-3'>
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									type='text'
+									value={password}
+									placeholder='Password'
+									onChange={(e) => setPassword(e.target.value)}
+								/>
+								<Form.Text className='text-muted'>
+									Your password requires a minimum of 8 characters.
+								</Form.Text>
+							</Form.Group>
+						</Col>
+						<Col>
+							<Form.Group controlId='registerEmail' className='mb-3'>
+								<Form.Label>Email</Form.Label>
+								<Form.Control
+									type='text'
+									value={email}
+									placeholder='Email'
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+								<Form.Text className='text-muted'>
+									Your email must be a valid email address.
+								</Form.Text>
+							</Form.Group>
+						</Col>
+					</Row>
+					<div className='d-grid gap-2 custom-button'>
+						<Button
+							variant='info primary'
+							type='submit'
+							size='sm'
+							onClick={handleSubmit}
+							block
+						>
+							Submit
+						</Button>
+						<Button
+							variant='danger primary'
+							type='secondary'
+							size='sm'
+							block
+							onClick={() => {
+								onBackClick(null);
+							}}
+						>
+							Back
+						</Button>
+					</div>
+				</Form>
+			</div>
+		</Container>
 	);
 }
 
