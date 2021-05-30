52c06b524a4fada50dda767d5168c0dcdae552cb
sirius6323 <sirius6323@gmail.com>
2021-05-30 09:24:14 -0700

    Removed Buttons and placed them in movie-view

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -20,16 +20,10 @@ export class MovieCard extends React.Component {
 						<Button
 							onClick={() => onMovieClick(movie)}
 							variant='danger link'
-							size='md'
+							size='sm'
 							block
 						>
 							Movie Info
-						</Button>
-						<Button className='mb-2' block variant='danger'>
-							Director
-						</Button>
-						<Button className='mb-2' block variant='danger'>
-							Genre
 						</Button>
 					</Card.Body>
 				</Card>
