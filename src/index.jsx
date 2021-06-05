ce31058c4b04b6e915c0e605e974e464f0e41fe1
sirius6323 <sirius6323@gmail.com>
2021-06-04 23:28:01 -0700

    Refactored import Container code

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
-import Container from 'react-bootstrap/Container';
+import { Container } from 'react-bootstrap';
 
 // Import statement to bundle './index.scss'
 import './index.scss';
