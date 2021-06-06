e8d215eced2d984cf221883c0e807bdba92af435
sirius6323 <sirius6323@gmail.com>
2021-06-06 13:42:07 -0700

    Parcel build

--- dist/index.02675e63.js
+++ dist/index.02675e63.js
@@ -26460,7 +26460,9 @@ try {
             columnNumber: 8
           }
         })) : movies.map(movie => /*#__PURE__*/_reactDefault.default.createElement(_reactBootstrap.Col, {
+          sm: 6,
           md: 4,
+          lg: 3,
           key: movie._id,
           __self: this,
           __source: {
