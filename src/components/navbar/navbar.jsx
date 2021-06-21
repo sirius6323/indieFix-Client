98abec27af5d2ee477f1752d77a7b0ba1526ba19
sirius6323 <sirius6323@gmail.com>
2021-06-21 12:03:45 -0700

    Added NavBar component

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,63 @@
+// Modules
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+// React Bootstrap Components
+import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+// Styles sheet
+import './navbar.scss';
+
+const logOut = () => {
+	localStorage.clear();
+	user({
+		user: null,
+		token: null,
+	});
+	window.open('/', '_self');
+};
+
+export class NavBar extends React.Component {
+	render() {
+		const { logOut, user } = this.props;
+
+		return (
+			<>
+				<Navbar
+					className='font-weight-bold'
+					fixed='top'
+					expand='sm'
+					bg='info'
+					variant='dark'
+				>
+					<Navbar.Brand href='/'>indieFix</Navbar.Brand>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+					<Navbar.Collapse bg='info' className='justify-content-end'>
+						<Nav className='d-flex navbar-bg'>
+							<Link
+								to='/profile'
+								className='text-white mx-4 my-2'
+								style={{ textDecoration: 'none' }}
+							>
+								Profile
+							</Link>
+							<Link
+								to='/'
+								className='text-white mx-4 my-2'
+								style={{ textDecoration: 'none' }}
+								onClick={logOut}
+							>
+								Log Out
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+			</>
+		);
+	}
+}
+
+NavBar.propTypes = {
+	logOut: PropTypes.func,
+};
