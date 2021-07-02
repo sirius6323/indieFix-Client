1b4a113c3d6347d65f65022da128fa348055b6d7
sirius6323 <sirius6323@gmail.com>
2021-07-02 00:49:09 -0700

    Refactored profile-view with added axios methods

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,102 +4,202 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './profile-view.scss';
+import ParaNorman from 'url:../images/paraNorman.jpg';
 
 export class ProfileView extends React.Component {
-	render() {
-		let { user, token, history, userData, onNewUser, onSignOut } = this.props;
+	constructor(props) {
+		super(props);
+		this.state = {
+			FirstName: '',
+			LastName: '',
+			Birthday: '',
+			Username: '',
+			Password: '',
+			Email: '',
+			FavoriteMovies: [],
+			FirstNameError: '',
+			LastNameError: '',
+			BirthdayError: '',
+			UsernameError: '',
+			PasswordError: '',
+			EmailError: '',
+		};
+	}
 
-		function updateInfo(token) {
-			const userFirst = document.getElementById('firstName');
-			const userLast = document.getElementById('lastName');
-			const birthdayInput = document.getElementById('birthday');
-			const userInput = document.getElementById('username');
-			const passInput = document.getElementById('password');
-			const passVerInput = document.getElementById('passwordVer');
-			const emailInput = document.getElementById('email');
+	componentDidMount() {
+		let accessToken = localStorage.getItem('token');
+		this.getUsers(accessToken);
+	}
 
-			if (userInput.value.length > 12) {
-				const userErr = document.getElementById('user');
-				return (userErr.innerText = 'Username can only be 12 characters');
-			}
+	getUsers(token) {
+		axios
+			.get('https://indiefix.herokuapp.com/users', {
+				headers: { Authorization: `Bearer ${token}` },
+			})
+			.then((response) => {
+				this.setState({
+					users: response.data,
+				});
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
 
-			const userNameChoice = userInput.value || UserData.Username;
-			let passChoice = null;
-			if (passInput.value == '') {
-				passChoice = '';
-			} else {
-				passChoice = passInput.value;
-			}
-			const firstNameChoice = userFirst.value || UserData.FirstName;
-			const lastNameChoice = userLast.value || UserData.LastName;
-			const emailChoice = emailInput.value || UserData.Email;
-			const birthdayChoice = birthdayInput.value || UserData.Birthday;
+	removeFavoriteMovie(movie) {
+		const token = localStorage.getItem('token');
+		const url =
+			'https://indiefix.herokuapp.com/users' +
+			localStorage.getItem('user') +
+			'/movies/' +
+			movie._id;
+		axios
+			.delete(url, {
+				headers: { Authorization: `Bearer ${token}` },
+			})
+			.then((response) => {
+				console.log(response);
+				this.componentDidMount();
 
-			if (passInput.value === passVerInput.value) {
-				axios
-					.put(
-						`https://indiefix.herokuapp.com/users/${user}`,
-						{
-							FirstName: firstNameChoice,
-							LastName: lastNameChoice,
-							Username: userNameChoice,
-							Password: passChoice,
-							Email: emailChoice,
-							Birthday: birthdayChoice,
-						},
-						{
-							headers: {
-								'Content-Type': 'application/json',
-								Authorization: `Bearer ${token}`,
-							},
-						}
-					)
-					.then((response) => {
-						console.log('Success with updating your account information');
-						let userData2 = response.data;
-						onNewUser(userData2);
-						if (userInput.value != '') {
-							window.location = `/users/${userData2.Username}`;
-						}
-						if (passChoice != '') {
-							window.location = `/users/${userData2.Username}`;
-						}
-					})
-					.catch(function (error) {
-						console.log(error);
-					});
-			} else {
-				const passErr = document.getElementById('password');
-				const passErrVer = document.getElementById('passwordVer');
-				passErr.innerText = 'Your password must match';
-				passErrVer.innerText = 'The passwords entered must match';
-			}
-		}
+				alert(`${movie.Title} has been removed from your Favorites List`);
+			});
+	}
 
-		function deleteAccount(token) {
-			console.log('Your account is not deleted yet');
+	handleDelete() {
+		const token = localStorage.getItem('token');
+		const user = localStorage.getItem('user');
+		axios
+			.delete(`https://indiefix.herokuapp.com/users/${user}`, {
+				headers: { Authorization: `Bearer ${token}` },
+			})
+			.then(() => {
+				alert(`${user} has been deleted`);
+				localStorage.removeItem('user');
+				localStorage.removeItem('token');
+				window.location.pathname = '/';
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
+
+	handleUpdate(e) {
+		const token = localStorage.getItem('token');
+		const user = localStorage.getItem('user');
+		console.log(this.state);
+		let formIsValid = this.formValidation();
+
+		if (formIsValid) {
 			axios
-				.delete(`https://indiefix.herokuapp.com/users/${user}`, {
-					headers: {
-						'Content-Type': 'application/json',
-						Authorization: `Bearer ${token}`,
+				.put(
+					`https://indiefix.herokuapp.com/users/${user}`,
+					{
+						FirstName: this.state.FirstName,
+						LastName: this.state.LastName,
+						Birthday: this.state.Birthday,
+						Username: this.state.Username,
+						Password: this.state.Password,
+						Email: this.state.Email,
 					},
-				})
+					{ headers: { Authorization: `Bearer ${token}` } }
+				)
 				.then((response) => {
+					const data = response.data;
+					localStorage.setItem('user', data.Username);
+					console.log(data);
+					alert(`${user} has been updated`);
 					console.log(response);
-					console.log(`${user} has been deleted from the database`);
 				})
-				.catch((e) => {
-					console.log('There was an error');
-					console.log(e);
+				.catch(function (error) {
+					console.log(error.response.data);
 				});
 		}
+	}
 
-		function Date() {
-			const formatDate = userData.Birthday;
+	formValidation() {
+		let FirstNameError = {};
+		let LastNameError = {};
+		let BirthdayError = {};
+		let UsernameError = {};
+		let PasswordError = {};
+		let EmailError = {};
+		let isValid = true;
 
-			return formatDate.slice(0, 10);
+		// First Name validation
+		if (this.state.FirstName.trim().length < 1) {
+			FirstNameError.firstNameShort =
+				'First Name must be alphanumeric and contain at least more then 1 character.';
+			isValid = false;
 		}
+		// Last Name validation
+		if (this.state.LastName.trim().length < 1) {
+			LastNameError.lastNameShort =
+				'Last Name must be alphanumeric and contain at least more then 1 character.';
+			isValid = false;
+		}
+		// Birthday validation
+		if (this.state.Birthday === '') {
+			BirthdayError.birthdayEmpty = 'Please enter your birthdate.';
+			isValid = false;
+		}
+		// Username validation
+		if (this.state.Username.trim().length < 5) {
+			UsernameError.usernameShort =
+				'Username must be alphanumeric and contain at least 5 characters.';
+			isValid = false;
+		}
+		// Password validation
+		if (this.state.Password.trim().length < 8) {
+			PasswordError.passwordMissing =
+				'You must enter your current password or a new password (minimum of 8 characters).';
+			isValid = false;
+		}
+		// Email validation
+		if (
+			!(
+				this.state.Email &&
+				this.state.Email.includes('.') &&
+				this.state.Email.includes('@')
+			)
+		) {
+			EmailError.notValidEmail = 'Please enter a valid email address.';
+			isValid = false;
+		}
+
+		this.setState({
+			FirstNameError: FirstNameError,
+			LastNameError: LastNameError,
+			BirthdayError: BirthdayError,
+			UsernameError: UsernameError,
+			PasswordError: PasswordError,
+			EmailError: EmailError,
+		});
+
+		return isValid;
+	}
+
+	handleChange(e) {
+		let { name, value } = e.target;
+
+		this.setState({
+			[name]: value,
+		});
+	}
+
+	render() {
+		const { user, movies } = this.props;
+		const {
+			FirstNameError,
+			LastNameError,
+			BirthdayError,
+			UsernameError,
+			PasswordError,
+			EmailError,
+		} = this.state;
+		const FavoriteMovieList = movies.filter((movie) => {
+			return this.state.FavoriteMovies.includes(movie._id);
+		});
 
 		return (
 			<Container>
@@ -115,8 +215,17 @@ export class ProfileView extends React.Component {
 									<Form.Control
 										type='text'
 										name='FirstName'
+										value={this.state.FirstName}
+										onChange={(e) => this.handleChange(e)}
 										placeholder='First Name'
 									/>
+									{Object.keys(FirstNameError).map((key) => {
+										return (
+											<div key={key} style={{ color: '#e50000' }}>
+												{FirstNameError[key]}
+											</div>
+										);
+									})}
 								</Form.Group>
 							</Col>
 							<Col>
@@ -125,8 +234,17 @@ export class ProfileView extends React.Component {
 									<Form.Control
 										type='text'
 										name='LastName'
+										value={this.state.LastName}
+										onChange={(e) => this.handleChange(e)}
 										placeholder='Last Name'
 									/>
+									{Object.keys(LastNameError).map((key) => {
+										return (
+											<div key={key} style={{ color: '#e50000' }}>
+												{LastNameError[key]}
+											</div>
+										);
+									})}
 								</Form.Group>
 							</Col>
 						</Row>
@@ -137,8 +255,17 @@ export class ProfileView extends React.Component {
 									<Form.Control
 										type='text'
 										name='Birthday'
+										value={this.state.Birthday}
+										onChange={(e) => this.handleChange(e)}
 										placeholder='MM/DD/YY'
 									/>
+									{Object.keys(BirthdayError).map((key) => {
+										return (
+											<div key={key} style={{ color: '#e50000' }}>
+												{BirthdayError[key]}
+											</div>
+										);
+									})}
 								</Form.Group>
 							</Col>
 							<Col>
@@ -147,8 +274,17 @@ export class ProfileView extends React.Component {
 									<Form.Control
 										type='text'
 										name='Username'
+										value={this.state.Username}
+										onChange={(e) => this.handleChange(e)}
 										placeholder='Username'
 									/>
+									{Object.keys(UsernameError).map((key) => {
+										return (
+											<div key={key} style={{ color: '#e50000' }}>
+												{UsernameError[key]}
+											</div>
+										);
+									})}
 								</Form.Group>
 							</Col>
 						</Row>
@@ -159,8 +295,17 @@ export class ProfileView extends React.Component {
 									<Form.Control
 										type='text'
 										name='Password'
+										value={this.state.Password}
+										onChange={(e) => this.handleChange(e)}
 										placeholder='Password'
 									/>
+									{Object.keys(PasswordError).map((key) => {
+										return (
+											<div key={key} style={{ color: '#e50000' }}>
+												{PasswordError[key]}
+											</div>
+										);
+									})}
 									<Form.Text className='text-muted'>
 										Your password requires a minimum of 8 characters.
 									</Form.Text>
@@ -169,7 +314,20 @@ export class ProfileView extends React.Component {
 							<Col>
 								<Form.Group className='mb-3'>
 									<Form.Label htmlFor='Email'>Email</Form.Label>
-									<Form.Control type='text' name='Email' placeholder='Email' />
+									<Form.Control
+										type='text'
+										name='Email'
+										value={this.state.Email}
+										onChange={(e) => this.handleChange(e)}
+										placeholder='Email'
+									/>
+									{Object.keys(EmailError).map((key) => {
+										return (
+											<div key={key} style={{ color: '#e50000' }}>
+												{EmailError[key]}
+											</div>
+										);
+									})}
 									<Form.Text className='text-muted'>
 										Your email must be a valid email address.
 									</Form.Text>
@@ -183,9 +341,7 @@ export class ProfileView extends React.Component {
 								type='submit'
 								expand='lg'
 								block
-								onClick={() => {
-									updateInfo(token);
-								}}
+								onClick={(e) => this.handleUpdate(e)}
 							>
 								Update
 							</Button>
@@ -195,9 +351,38 @@ export class ProfileView extends React.Component {
 								type='button'
 								size='sm'
 								block
+								onClick={() => this.handleDelete()}
 							>
 								Delete Account
 							</Button>
+						</div>
+						<div style={{ float: 'center', textAlign: 'center' }}>
+							<Card.Text className='mt-3' as='h3'>
+								Favorite Movies:
+							</Card.Text>
+							<Row>
+								{FavoriteMovieList.map((movie) => {
+									return (
+										<Col md={3} key={movie._id}>
+											<div key={movie._id}>
+												<Card>
+													<Card.Img variant='top' src={ParaNorman} />
+													<Card.Body>
+														<Link to={`/movies/${movie._id}`}>
+															<Card.Title as='h6'>{movie.Title}</Card.Title>
+														</Link>
+														<Button
+															onClick={() => this.removeFavoriteMovie(movie)}
+														>
+															Remove Movie
+														</Button>
+													</Card.Body>
+												</Card>
+											</div>
+										</Col>
+									);
+								})}
+							</Row>
 						</div>
 					</Form>
 				</div>
@@ -205,3 +390,7 @@ export class ProfileView extends React.Component {
 		);
 	}
 }
+
+ProfileView.propTypes = {
+	movies: PropTypes.array.isRequired,
+};
